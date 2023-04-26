9abb88e986104d26fea8e1a79b0bba480c022a8a
AntonFartovii <anton@rza.by>
2023-04-26 13:53:26 +0300

    feat: keys database

---
+++ data/keyboard.js
@@ -0,0 +1,246 @@
+
+export const keyboard = {
+        1: [
+            {
+                en: {value: '`', shiftValue: '~'},
+                ru: {value: 'ё', shiftValue: 'Ё'}
+            },
+            {
+                en: {value: '1', shiftValue: '!'},
+                ru: {value: '1', shiftValue: '!'}
+            },
+            {
+                en: {value: '2', shiftValue: '@'},
+                ru: {value: '2', shiftValue: '"'}
+            },
+            {
+                en: {value: '3', shiftValue: '#'},
+                ru: {value: '3', shiftValue: '№'}
+            },
+            {
+                en: {value: '4', shiftValue: '$'},
+                ru: {value: '4', shiftValue: ';'}
+            },
+            {
+                en: {value: '5', shiftValue: '%'},
+                ru: {value: '5', shiftValue: '%'}
+            },
+            {
+                en: {value: '6', shiftValue: '^'},
+                ru: {value: '6', shiftValue: ':'}
+            },
+            {
+                en: {value: '7', shiftValue: '&'},
+                ru: {value: '7', shiftValue: '?'}
+            },
+            {
+                en: {value: '8', shiftValue: '*'},
+                ru: {value: '8', shiftValue: '*'}
+            },
+            {
+                en: {value: '9', shiftValue: '('},
+                ru: {value: '9', shiftValue: '('}
+            },
+            {
+                en: {value: '0', shiftValue: ')'},
+                ru: {value: '0', shiftValue: ')'}
+            },
+            {
+                en: {value: '-', shiftValue: '_'},
+                ru: {value: '-', shiftValue: '_'}
+            },
+            {
+                en: {value: '=', shiftValue: '+'},
+                ru: {value: '=', shiftValue: '+'}
+            },
+            {
+                en: {value: 'Backspace', shiftValue: ''},
+                ru: {value: 'Убрать', shiftValue: ''}
+            }
+        ],
+        2: [
+            {
+                en: {value: 'Tab', shiftValue: ''},
+                ru: {value: 'Таб', shiftValue: ''}
+            },
+            {
+                en: {value: 'q', shiftValue: ''},
+                ru: {value: 'й', shiftValue: 'Й'}
+            },
+            {
+                en: {value: 'w', shiftValue: ''},
+                ru: {value: 'ц', shiftValue: 'Ц'}
+            },
+            {
+                en: {value: 'e', shiftValue: ''},
+                ru: {value: 'у', shiftValue: 'У'}
+            },
+            {
+                en: {value: 'r', shiftValue: ''},
+                ru: {value: 'к', shiftValue: 'К'}
+            },
+            {
+                en: {value: 't', shiftValue: ''},
+                ru: {value: 'е', shiftValue: 'Е'}
+            },
+            {
+                en: {value: 'y', shiftValue: ''},
+                ru: {value: 'н', shiftValue: 'Н'}
+            },
+            {
+                en: {value: 'u', shiftValue: ''},
+                ru: {value: 'г', shiftValue: 'Г'}
+            },
+            {
+                en: {value: 'i', shiftValue: ''},
+                ru: {value: 'ш', shiftValue: 'Ш'}
+            },
+            {
+                en: {value: 'o', shiftValue: ''},
+                ru: {value: 'щ', shiftValue: 'Щ'}
+            },
+            {
+                en: {value: 'p', shiftValue: ''},
+                ru: {value: 'з', shiftValue: 'З'}
+            },
+            {
+                en: {value: '[', shiftValue: '{'},
+                ru: {value: 'х', shiftValue: 'Х'}
+            },
+            {
+                en: {value: ']', shiftValue: '}'},
+                ru: {value: 'ъ', shiftValue: 'Ъ'}
+            },
+            {
+                en: {value: '\\', shiftValue: '|'},
+                ru: {value: '\\', shiftValue: '/'}
+            }
+
+        ],
+        3: [
+            {
+                en: {value: 'Caps', shiftValue: ''},
+                ru: {value: 'Caps', shiftValue: ''}
+            },
+            {
+                en: {value: 'a', shiftValue: ''},
+                ru: {value: 'ф', shiftValue: ''}
+            },
+            {
+                en: {value: 's', shiftValue: ''},
+                ru: {value: 'ы', shiftValue: ''}
+            },
+            {
+                en: {value: 'd', shiftValue: ''},
+                ru: {value: 'в', shiftValue: ''}
+            },
+            {
+                en: {value: 'f', shiftValue: ''},
+                ru: {value: 'а', shiftValue: ''}
+            },
+            {
+                en: {value: 'g', shiftValue: ''},
+                ru: {value: 'п', shiftValue: ''}
+            },
+            {
+                en: {value: 'h', shiftValue: ''},
+                ru: {value: 'р', shiftValue: ''}
+            },
+            {
+                en: {value: 'j', shiftValue: ''},
+                ru: {value: 'о', shiftValue: ''}
+            },
+            {
+                en: {value: 'k', shiftValue: ''},
+                ru: {value: 'л', shiftValue: ''}
+            },
+            {
+                en: {value: 'l', shiftValue: ''},
+                ru: {value: 'д', shiftValue: ''}
+            },
+            {
+                en: {value: ';', shiftValue: ':'},
+                ru: {value: 'ж', shiftValue: 'Ж'}
+            },
+            {
+                en: {value: `'`, shiftValue: '"'},
+                ru: {value: 'э', shiftValue: 'Э'}
+            },
+            {
+                en: {value: `Enter`, shiftValue: ''},
+                ru: {value: 'Ввод', shiftValue: ''}
+            }
+        ],
+        4: [
+            {
+              en: {value: `Shift`, shiftValue: ''},
+              ru: {value: 'Shift', shiftValue: ''}
+            },
+            {
+              en: {value: 'z', shiftValue: 'Z'},
+              ru: {value: '', shiftValue: ''}
+            },
+            {
+              en: {value: 'x', shiftValue: 'X'},
+              ru: {value: '', shiftValue: ''}
+            },
+            {
+              en: {value: 'c', shiftValue: 'C'},
+              ru: {value: '', shiftValue: ''}
+            },
+            {
+              en: {value: 'v', shiftValue: 'V'},
+              ru: {value: '', shiftValue: ''}
+            },
+            {
+              en: {value: 'b', shiftValue: 'B'},
+              ru: {value: '', shiftValue: ''}
+            },
+            {
+              en: {value: 'n', shiftValue: 'N'},
+              ru: {value: '', shiftValue: ''}
+            },
+            {
+                en: {value: 'm', shiftValue: 'M'},
+                ru: {value: '', shiftValue: ''}
+            },
+            {
+                en: {value: ',', shiftValue: '<'},
+                ru: {value: 'б', shiftValue: 'Б'}
+            },
+            {
+                en: {value: '.', shiftValue: '>'},
+                ru: {value: 'ю', shiftValue: 'Ю'}
+            },
+            {
+                en: {value: '/', shiftValue: '?'},
+                ru: {value: '.', shiftValue: ','}
+            }
+        ],
+        5: [
+            {
+                en: {value: `Ctrl`, shiftValue: ''},
+                ru: {value: 'Ctrl', shiftValue: ''}
+            },
+            {
+                en: {value: `Alt`, shiftValue: ''},
+                ru: {value: 'Alt', shiftValue: ''}
+            },
+            {
+                en: {value: ``, shiftValue: ''},
+                ru: {value: '', shiftValue: ''}
+            },
+            {
+                en: {value: `&#8592;`, shiftValue: ''},
+                ru: {value: '&#8592;', shiftValue: ''}
+            },,
+            {
+                en: {value: `&#8595;`, shiftValue: ''},
+                ru: {value: '&#8595;', shiftValue: ''}
+            },,
+            {
+                en: {value: `&#8594;`, shiftValue: ''},
+                ru: {value: '&#8594;', shiftValue: ''}
+            }
+        ]
+}
