f68ebb0ac2d495d7148c9d6af4b8008bfada1c21
AntonFartovii <anton@rza.by>
2023-04-26 19:32:47 +0300

    refactor: database

--- data/keyboard.js
+++ data/keyboard.js
@@ -3,244 +3,332 @@ export const keyboard = {
         1: [
             {
                 en: {value: '`', shiftValue: '~'},
-                ru: {value: 'ё', shiftValue: 'Ё'}
+                ru: {value: 'ё', shiftValue: ''},
+                className: ''
             },
             {
                 en: {value: '1', shiftValue: '!'},
-                ru: {value: '1', shiftValue: '!'}
+                ru: {value: '1', shiftValue: '!'},
+                className: ''
             },
             {
                 en: {value: '2', shiftValue: '@'},
-                ru: {value: '2', shiftValue: '"'}
+                ru: {value: '2', shiftValue: '"'},
+                className: ''
             },
             {
                 en: {value: '3', shiftValue: '#'},
-                ru: {value: '3', shiftValue: '№'}
+                ru: {value: '3', shiftValue: '№'},
+                className: ''
             },
             {
                 en: {value: '4', shiftValue: '$'},
-                ru: {value: '4', shiftValue: ';'}
+                ru: {value: '4', shiftValue: ';'},
+                className: ''
             },
             {
                 en: {value: '5', shiftValue: '%'},
-                ru: {value: '5', shiftValue: '%'}
+                ru: {value: '5', shiftValue: '%'},
+                className: ''
             },
             {
                 en: {value: '6', shiftValue: '^'},
-                ru: {value: '6', shiftValue: ':'}
+                ru: {value: '6', shiftValue: ':'},
+                className: ''
             },
             {
                 en: {value: '7', shiftValue: '&'},
-                ru: {value: '7', shiftValue: '?'}
+                ru: {value: '7', shiftValue: '?'},
+                className: ''
             },
             {
                 en: {value: '8', shiftValue: '*'},
-                ru: {value: '8', shiftValue: '*'}
+                ru: {value: '8', shiftValue: '*'},
+                className: ''
             },
             {
                 en: {value: '9', shiftValue: '('},
-                ru: {value: '9', shiftValue: '('}
+                ru: {value: '9', shiftValue: '('},
+                className: ''
             },
             {
                 en: {value: '0', shiftValue: ')'},
-                ru: {value: '0', shiftValue: ')'}
+                ru: {value: '0', shiftValue: ')'},
+                className: ''
             },
             {
                 en: {value: '-', shiftValue: '_'},
-                ru: {value: '-', shiftValue: '_'}
+                ru: {value: '-', shiftValue: '_'},
+                className: ''
             },
             {
                 en: {value: '=', shiftValue: '+'},
-                ru: {value: '=', shiftValue: '+'}
+                ru: {value: '=', shiftValue: '+'},
+                className: ''
             },
             {
                 en: {value: 'Backspace', shiftValue: ''},
-                ru: {value: 'Убрать', shiftValue: ''}
+                ru: {value: 'Убрать', shiftValue: ''},
+                className: 'backspace'
             }
         ],
         2: [
             {
                 en: {value: 'Tab', shiftValue: ''},
-                ru: {value: 'Таб', shiftValue: ''}
+                ru: {value: 'Таб', shiftValue: ''},
+                className: 'Tab'
             },
             {
                 en: {value: 'q', shiftValue: ''},
-                ru: {value: 'й', shiftValue: 'Й'}
+                ru: {value: 'й', shiftValue: ''},
+                className: ''
             },
             {
                 en: {value: 'w', shiftValue: ''},
-                ru: {value: 'ц', shiftValue: 'Ц'}
+                ru: {value: 'ц', shiftValue: ''},
+                className: ''
             },
             {
                 en: {value: 'e', shiftValue: ''},
-                ru: {value: 'у', shiftValue: 'У'}
+                ru: {value: 'у', shiftValue: ''},
+                className: ''
             },
             {
                 en: {value: 'r', shiftValue: ''},
-                ru: {value: 'к', shiftValue: 'К'}
+                ru: {value: 'к', shiftValue: ''},
+                className: ''
             },
             {
                 en: {value: 't', shiftValue: ''},
-                ru: {value: 'е', shiftValue: 'Е'}
+                ru: {value: 'е', shiftValue: ''},
+                className: ''
             },
             {
                 en: {value: 'y', shiftValue: ''},
-                ru: {value: 'н', shiftValue: 'Н'}
+                ru: {value: 'н', shiftValue: ''},
+                className: ''
             },
             {
                 en: {value: 'u', shiftValue: ''},
-                ru: {value: 'г', shiftValue: 'Г'}
+                ru: {value: 'г', shiftValue: ''},
+                className: ''
             },
             {
                 en: {value: 'i', shiftValue: ''},
-                ru: {value: 'ш', shiftValue: 'Ш'}
+                ru: {value: 'ш', shiftValue: ''},
+                className: ''
             },
             {
                 en: {value: 'o', shiftValue: ''},
-                ru: {value: 'щ', shiftValue: 'Щ'}
+                ru: {value: 'щ', shiftValue: ''},
+                className: ''
             },
             {
                 en: {value: 'p', shiftValue: ''},
-                ru: {value: 'з', shiftValue: 'З'}
+                ru: {value: 'з', shiftValue: ''},
+                className: ''
             },
             {
                 en: {value: '[', shiftValue: '{'},
-                ru: {value: 'х', shiftValue: 'Х'}
+                ru: {value: 'х', shiftValue: ''},
+                className: ''
             },
             {
                 en: {value: ']', shiftValue: '}'},
-                ru: {value: 'ъ', shiftValue: 'Ъ'}
+                ru: {value: 'ъ', shiftValue: ''},
+                className: ''
             },
             {
                 en: {value: '\\', shiftValue: '|'},
-                ru: {value: '\\', shiftValue: '/'}
+                ru: {value: '\\', shiftValue: '/'},
+                className: ''
+            },
+            {
+                en: {value: 'Del', shiftValue: ''},
+                ru: {value: 'Del', shiftValue: ''},
+                className: 'delete'
             }
 
         ],
         3: [
             {
-                en: {value: 'Caps', shiftValue: ''},
-                ru: {value: 'Caps', shiftValue: ''}
+                en: {value: 'CapsLock', shiftValue: ''},
+                ru: {value: 'CapsLock', shiftValue: ''},
+                className: 'CapsLock'
             },
             {
                 en: {value: 'a', shiftValue: ''},
-                ru: {value: 'ф', shiftValue: ''}
+                ru: {value: 'ф', shiftValue: ''},
+                className: ''
             },
             {
                 en: {value: 's', shiftValue: ''},
-                ru: {value: 'ы', shiftValue: ''}
+                ru: {value: 'ы', shiftValue: ''},
+                className: ''
             },
             {
                 en: {value: 'd', shiftValue: ''},
-                ru: {value: 'в', shiftValue: ''}
+                ru: {value: 'в', shiftValue: ''},
+                className: ''
             },
             {
                 en: {value: 'f', shiftValue: ''},
-                ru: {value: 'а', shiftValue: ''}
+                ru: {value: 'а', shiftValue: ''},
+                className: ''
             },
             {
                 en: {value: 'g', shiftValue: ''},
-                ru: {value: 'п', shiftValue: ''}
+                ru: {value: 'п', shiftValue: ''},
+                className: ''
             },
             {
                 en: {value: 'h', shiftValue: ''},
-                ru: {value: 'р', shiftValue: ''}
+                ru: {value: 'р', shiftValue: ''},
+                className: ''
             },
             {
                 en: {value: 'j', shiftValue: ''},
-                ru: {value: 'о', shiftValue: ''}
+                ru: {value: 'о', shiftValue: ''},
+                className: ''
             },
             {
                 en: {value: 'k', shiftValue: ''},
-                ru: {value: 'л', shiftValue: ''}
+                ru: {value: 'л', shiftValue: ''},
+                className: ''
             },
             {
                 en: {value: 'l', shiftValue: ''},
-                ru: {value: 'д', shiftValue: ''}
+                ru: {value: 'д', shiftValue: ''},
+                className: ''
             },
             {
                 en: {value: ';', shiftValue: ':'},
-                ru: {value: 'ж', shiftValue: 'Ж'}
+                ru: {value: 'ж', shiftValue: ''},
+                className: ''
             },
             {
                 en: {value: `'`, shiftValue: '"'},
-                ru: {value: 'э', shiftValue: 'Э'}
+                ru: {value: 'э', shiftValue: ''},
+                className: ''
             },
             {
                 en: {value: `Enter`, shiftValue: ''},
-                ru: {value: 'Ввод', shiftValue: ''}
+                ru: {value: 'Ввод', shiftValue: ''},
+                className: 'Enter'
             }
         ],
         4: [
             {
               en: {value: `Shift`, shiftValue: ''},
-              ru: {value: 'Shift', shiftValue: ''}
+              ru: {value: 'Shift', shiftValue: ''},
+                className: 'Shift shift-left'
             },
             {
-              en: {value: 'z', shiftValue: 'Z'},
-              ru: {value: '', shiftValue: ''}
+              en: {value: 'z', shiftValue: ''},
+              ru: {value: '', shiftValue: ''},
+                className: ''
             },
             {
-              en: {value: 'x', shiftValue: 'X'},
-              ru: {value: '', shiftValue: ''}
+              en: {value: 'x', shiftValue: ''},
+              ru: {value: '', shiftValue: ''},
+                className: ''
             },
             {
-              en: {value: 'c', shiftValue: 'C'},
-              ru: {value: '', shiftValue: ''}
+              en: {value: 'c', shiftValue: ''},
+              ru: {value: '', shiftValue: ''},
+                className: ''
             },
             {
-              en: {value: 'v', shiftValue: 'V'},
-              ru: {value: '', shiftValue: ''}
+              en: {value: 'v', shiftValue: ''},
+              ru: {value: '', shiftValue: ''},
+                className: ''
             },
             {
-              en: {value: 'b', shiftValue: 'B'},
-              ru: {value: '', shiftValue: ''}
+              en: {value: 'b', shiftValue: ''},
+              ru: {value: '', shiftValue: ''},
+                className: ''
             },
             {
-              en: {value: 'n', shiftValue: 'N'},
-              ru: {value: '', shiftValue: ''}
+              en: {value: 'n', shiftValue: ''},
+              ru: {value: '', shiftValue: ''},
+                className: ''
             },
             {
-                en: {value: 'm', shiftValue: 'M'},
-                ru: {value: '', shiftValue: ''}
+                en: {value: 'm', shiftValue: ''},
+                ru: {value: '', shiftValue: ''},
+                className: ''
             },
             {
                 en: {value: ',', shiftValue: '<'},
-                ru: {value: 'б', shiftValue: 'Б'}
+                ru: {value: 'б', shiftValue: ''},
+                className: ''
             },
             {
                 en: {value: '.', shiftValue: '>'},
-                ru: {value: 'ю', shiftValue: 'Ю'}
+                ru: {value: 'ю', shiftValue: ''},
+                className: ''
             },
             {
                 en: {value: '/', shiftValue: '?'},
-                ru: {value: '.', shiftValue: ','}
+                ru: {value: '.', shiftValue: ','},
+                className: ''
+            },
+            {
+                en: {value: '&#8593;', shiftValue: ''},
+                ru: {value: '&#8593;', shiftValue: ''},
+                className: 'arrow arrow-up'
+            },
+            {
+                en: {value: `Shift`, shiftValue: ''},
+                ru: {value: 'Shift', shiftValue: ''},
+                className: 'Shift shift-right'
             }
         ],
         5: [
             {
                 en: {value: `Ctrl`, shiftValue: ''},
-                ru: {value: 'Ctrl', shiftValue: ''}
-            },
-            {
-                en: {value: `Alt`, shiftValue: ''},
-                ru: {value: 'Alt', shiftValue: ''}
+                ru: {value: 'Ctrl', shiftValue: ''},
+                className: 'control control-left'
             },
             {
                 en: {value: ``, shiftValue: ''},
-                ru: {value: '', shiftValue: ''}
+                ru: {value: '', shiftValue: ''},
+                className: 'windows'
+            },
+            {
+                en: {value: `Alt`, shiftValue: ''},
+                ru: {value: 'Alt', shiftValue: ''},
+                className: 'Alt'
+            },
+            {
+                en: {value: ``, shiftValue: ''},
+                ru: {value: '', shiftValue: ''},
+                className: 'spacebar'
+            },
+            {
+                en: {value: `Alt`, shiftValue: ''},
+                ru: {value: 'Alt', shiftValue: ''},
+                className: 'Alt'
+            },
+            {
+                en: {value: `Ctrl`, shiftValue: ''},
+                ru: {value: 'Ctrl', shiftValue: ''},
+                className: 'control control-right'
             },
             {
                 en: {value: `&#8592;`, shiftValue: ''},
-                ru: {value: '&#8592;', shiftValue: ''}
-            },,
+                ru: {value: '&#8592;', shiftValue: ''},
+                className: 'arrow arrow-left'
+            },
             {
                 en: {value: `&#8595;`, shiftValue: ''},
-                ru: {value: '&#8595;', shiftValue: ''}
-            },,
+                ru: {value: '&#8595;', shiftValue: ''},
+                className: 'arrow arrow-down'
+            },
             {
                 en: {value: `&#8594;`, shiftValue: ''},
-                ru: {value: '&#8594;', shiftValue: ''}
+                ru: {value: '&#8594;', shiftValue: ''},
+                className: 'arrow arrow-right'
             }
         ]
 }
