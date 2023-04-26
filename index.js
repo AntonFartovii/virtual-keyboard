9f318659ce69bc348ce39d9bb72dc099d25db3cf
AntonFartovii <anton@rza.by>
2023-04-26 19:32:05 +0300

    init: store

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-import {App} from "./core/App.js";
+import {App} from "./components/App.js";
 import {Board} from "./components/Board.js";
 import {Textarea} from "./components/Textarea.js";
+import {storage} from "./core/utils.js";
 
+const defaultState = {
+    lang: 'en',
+    currentText: ''
+}
+
+export const initialState = storage('keyboard-state')
+    ? storage('keyboard-state')
+    : defaultState
 
 const app = new App (
     '#app',
