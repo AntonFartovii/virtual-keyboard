25fe9185f9e933dfc44e0d77c0bdd1241b24de1a
AntonFartovii <anton@rza.by>
2023-04-30 18:33:27 +0300

    feat: add eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,60 @@
 import path from 'path'
 import {getDirPath} from "./service.js";
 import HtmlWebpackPlugin from "html-webpack-plugin";
-import CopyPlugin from "copy-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {CleanWebpackPlugin} from "clean-webpack-plugin";
+import ESLintWebpackPlugin from "eslint-webpack-plugin";
+
 const url = import.meta.url
+const __dirname = getDirPath(url)
 
-const __dirname = getDirPath( url )
-
-export default {
-    context: path.resolve(__dirname,'src'),
-    entry: {
-        main: './index.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js'
-    },
-    resolve: {
-      extensions: ['.js','.json'],
-      alias: {
-          '@': path.resolve(__dirname, 'src'),
-          '@core': path.resolve(__dirname, 'src', 'core')
-      }
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html'
-        }),
-        new CopyPlugin({
-            patterns: [{
-                from: path.resolve(__dirname, 'src/assets/icons/windows-icon.svg' ),
-                to: path.resolve(__dirname, 'dist/assets/icons/' )
-            }]
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'bundle.css'
-        }),
-
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader'
-                ]
+const config = (env, args) => {
+    const isProd = args.mode === 'production'
+    const isDev = !isProd
+    const filename = ext => isProd ? `[name].[contenthash].${ext}`:`[name].bundle.${ext}`
+    const plugins = () => {
+        const base = [
+                new HtmlWebpackPlugin({
+                    template: './index.html'
+                }),
+                new MiniCssExtractPlugin({
+                    filename: filename('css')
+                }),
+                new CleanWebpackPlugin()
+            ]
+        if (isDev) {
+            base.push( new ESLintWebpackPlugin() )
+        }
+        return base
+    }
+    return {
+        context: path.resolve(__dirname, 'src'),
+        entry: {
+            main: './index.js'
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: filename('js')
+        },
+        resolve: {
+            extensions: ['.js', '.json'],
+            alias: {
+                '@': path.resolve(__dirname, 'src'),
+                '@core': path.resolve(__dirname, 'src', 'core')
             }
-        ]
+        },
+        plugins: plugins(),
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        'css-loader'
+                    ]
+                }
+            ]
+        }
     }
 }
+export default config
