e2bd03462550948cfe7546d3c94afae2ae69389c
AntonFartovii <anton@rza.by>
2023-05-02 18:58:58 +0300

    refactor: generate index.html

--- webpack.config.js
+++ webpack.config.js
@@ -14,14 +14,19 @@ const config = (env, args) => {
   const plugins = () => {
     const base = [
       new HtmlWebpackPlugin({
-        meta: {
-          'viewport': 'width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0',
-          'charset': 'UTF-8',
-          'http-equiv': {
-            'X-UA-Compatible': 'ie=edge'
-          }
-        },
-        template: './index.html'
+        inject: 'head',
+        templateContent: `<!doctype html>
+          <html lang="en">
+          <head>
+            <meta charset="UTF-8">
+            <meta name="viewport"
+                content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Document</title>
+          </head>
+          <body>
+          </body>
+          </html>`
       }),
       new MiniCssExtractPlugin({
         filename: filename('css')
